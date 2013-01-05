479b0c190d248cc92c4894f061f51c005e684dc0
Séverin Lemaignan <skadge@gmail.com>
2013-01-05 23:30:18 +0100

    Re-organized code into proper package, add a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+ # -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='freehand',
+      version='1.0',
+      license='GNU GPL',
+      description='A freehand drawing tool.',
+      author='L. Konneker',
+      url='https://github.com/bootchk/freehandTool',
+      package_dir = {'': 'src'},
+      packages=['freehand'],
+      )
